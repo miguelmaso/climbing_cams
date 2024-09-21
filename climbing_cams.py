8637c5458aed6c17089c28223e8d39051cb6993d
miguelmaso <miguel.maso@upc.edu>
2024-09-21 12:33:24 +0200

    moved to inner class

--- climbing_cams.py
+++ climbing_cams.py
@@ -155,78 +155,84 @@ def select_cams(data, brand="", name="", number="", color="", expansion_range=[]
             rack.append(cam)
     return rack
 
-def bar_chart(rack, ax=None, ylabel='[{number}]', number_inside=False):
-    if ax is None:
-        ax = plt.gca()
-    labels = [ylabel.format(brand=cam.brand, name=cam.name, number=cam.number) for cam in rack]
-    minimums = [cam.min for cam in rack]
-    maximums = [cam.max for cam in rack]
-    ranges = [maximum - minimum for maximum, minimum in zip(maximums, minimums)]
-    colors = [cam.color for cam in rack]
-    bars = ax.barh(labels, width=ranges, left=minimums, height=.8, color=colors, alpha=0.7)
+class Plot:
 
-    for patch in reversed(bars):
-        bb = patch.get_bbox()
-        color = patch.get_facecolor()
-        p_bbox = mpl.patches.FancyBboxPatch((bb.xmin, bb.ymin),
-                            abs(bb.width), abs(bb.height),
-                            boxstyle="round,pad=0,rounding_size=0.5",
-                            ec="none", fc=color,
-                            mutation_aspect=0.2
-                            )
-        patch.remove()
-        ax.add_patch(p_bbox)
+    @staticmethod
+    def bar_chart(rack, ax=None, ylabel='[{number}]', number_inside=False):
+        if ax is None:
+            ax = plt.gca()
+        labels = [ylabel.format(brand=cam.brand, name=cam.name, number=cam.number) for cam in rack]
+        minimums = [cam.min for cam in rack]
+        maximums = [cam.max for cam in rack]
+        ranges = [maximum - minimum for maximum, minimum in zip(maximums, minimums)]
+        colors = [cam.color for cam in rack]
+        bars = ax.barh(labels, width=ranges, left=minimums, height=.8, color=colors, alpha=0.7)
 
-    if number_inside:
-        numbers = [cam.number for cam in rack]
-        ax.bar_label(bars, numbers, label_type='center', fontsize=5, weight='bold', color='white')
+        for patch in reversed(bars):
+            bb = patch.get_bbox()
+            color = patch.get_facecolor()
+            p_bbox = mpl.patches.FancyBboxPatch((bb.xmin, bb.ymin),
+                                abs(bb.width), abs(bb.height),
+                                boxstyle="round,pad=0,rounding_size=0.5",
+                                ec="none", fc=color,
+                                mutation_aspect=0.2
+                                )
+            patch.remove()
+            ax.add_patch(p_bbox)
 
-def plot_ranges(racks_list, smart_ylabels=False, numbers_inside=False):
-    sizes = [len(rack) for rack in racks_list]
-    fig, axes = plt.subplots(nrows=len(racks_list), sharex=True,
-        gridspec_kw={'height_ratios':sizes, 'hspace':0})
-    axes = [axes] if len(racks_list) == 1 else axes
+        if number_inside:
+            numbers = [cam.number for cam in rack]
+            ax.bar_label(bars, numbers, label_type='center', fontsize=5, weight='bold', color='white')
 
-    for rack, ax in zip(racks_list, axes):
-        bar_chart(rack, ax, number_inside=numbers_inside)
-        sep = '\n'
-        ax.set_ylabel(f'{rack.name(sep)}')
-        ax.spines.right.set_visible(False)
-        ax.spines.left.set_visible(False)
-        ax.spines.top.set_visible(False)
-        ax.tick_params(length=0)
-        ax.xaxis.grid()
-        ax.set_axisbelow(True)
-        if smart_ylabels:
-            ax.set_yticklabels([])
-            ax.set_ylabel(f'{rack.name(sep)}', rotation=0, horizontalalignment='right', verticalalignment='center')
-    fig.tight_layout()
-    return fig, axes
+    @staticmethod
+    def plot_ranges(racks_list, smart_ylabels=False, numbers_inside=False):
+        sizes = [len(rack) for rack in racks_list]
+        fig, axes = plt.subplots(nrows=len(racks_list), sharex=True,
+            gridspec_kw={'height_ratios':sizes, 'hspace':0})
+        axes = [axes] if len(racks_list) == 1 else axes
 
-def scatter_average(racks, xvalue, yvalue, ax=None):
-    if not ax:
-        fig, ax = plt.subplots()
-    else:
-        fig = ax.get_figure()
-    for rack in racks:
-        ax.plot([getattr(rack, xvalue)], [getattr(rack, yvalue)], label=rack.name(), marker='o', markersize=10, linewidth=0, alpha=.7)
-        ax.legend()
-    ax.set_xlabel(f'{xvalue.replace("_"," ").capitalize()} [{getattr(Units, xvalue)}]')
-    ax.set_ylabel(f'{yvalue.replace("_"," ").capitalize()} [{getattr(Units, yvalue)}]')
-    fig.tight_layout()
-    return fig, ax
+        for rack, ax in zip(racks_list, axes):
+            Plot.bar_chart(rack, ax, number_inside=numbers_inside)
+            sep = '\n'
+            ax.set_ylabel(f'{rack.name(sep)}')
+            ax.spines.right.set_visible(False)
+            ax.spines.left.set_visible(False)
+            ax.spines.top.set_visible(False)
+            ax.tick_params(length=0)
+            ax.xaxis.grid()
+            ax.set_axisbelow(True)
+            if smart_ylabels:
+                ax.set_yticklabels([])
+                ax.set_ylabel(f'{rack.name(sep)}', rotation=0, horizontalalignment='right', verticalalignment='center')
+        fig.tight_layout()
+        return fig, axes
 
-def scatter_individual(racks, xvalue, yvalue, ax=None):
-    if not ax:
-        fig, ax = plt.subplots()
-    else:
-        fig = ax.get_figure()
-    for rack in racks:
-        x = [getattr(i, xvalue) for i in rack]
-        y = [getattr(i, yvalue) for i in rack]
-        ax.plot(x, y, label=rack.name(), marker='o', markersize=10, linewidth=0, alpha=.7)
-        ax.legend()
-    ax.set_xlabel(f'{xvalue.replace("_"," ").capitalize()} [{getattr(Units, xvalue)}]')
-    ax.set_ylabel(f'{yvalue.replace("_"," ").capitalize()} [{getattr(Units, yvalue)}]')
-    fig.tight_layout()
-    return fig, ax
+    @staticmethod
+    def scatter_average(racks, xvalue, yvalue, ax=None):
+        if not ax:
+            fig, ax = plt.subplots()
+        else:
+            fig = ax.get_figure()
+        for rack in racks:
+            ax.plot([getattr(rack, xvalue)], [getattr(rack, yvalue)], label=rack.name(), marker='o', markersize=10, linewidth=0, alpha=.7)
+            ax.legend()
+        ax.set_xlabel(f'{xvalue.replace("_"," ").capitalize()} [{getattr(Units, xvalue)}]')
+        ax.set_ylabel(f'{yvalue.replace("_"," ").capitalize()} [{getattr(Units, yvalue)}]')
+        fig.tight_layout()
+        return fig, ax
+
+    @staticmethod
+    def scatter_individual(racks, xvalue, yvalue, ax=None):
+        if not ax:
+            fig, ax = plt.subplots()
+        else:
+            fig = ax.get_figure()
+        for rack in racks:
+            x = [getattr(i, xvalue) for i in rack]
+            y = [getattr(i, yvalue) for i in rack]
+            ax.plot(x, y, label=rack.name(), marker='o', markersize=10, linewidth=0, alpha=.7)
+            ax.legend()
+        ax.set_xlabel(f'{xvalue.replace("_"," ").capitalize()} [{getattr(Units, xvalue)}]')
+        ax.set_ylabel(f'{yvalue.replace("_"," ").capitalize()} [{getattr(Units, yvalue)}]')
+        fig.tight_layout()
+        return fig, ax
--- examples/comparison.py
+++ examples/comparison.py
@@ -15,11 +15,11 @@ families_specifications = [
     {'brand':'Wild Country', 'name':'Friend'}
 ]
 families = [cams.select_cams(data, **spec) for spec in families_specifications]
-cams.plot_ranges(families, smart_ylabels=True, numbers_inside=True)
-cams.scatter_average(families, 'expansion_rate', 'specific_weight')
+cams.Plot.plot_ranges(families, smart_ylabels=True, numbers_inside=True)
+cams.Plot.scatter_average(families, 'expansion_rate', 'specific_weight')
 fig, ax = plt.subplots(2,1)
-cams.scatter_individual(families, 'avg', 'weight', ax[0])
-cams.scatter_individual(families, 'avg', 'specific_weight', ax[1])
+cams.Plot.scatter_individual(families, 'avg', 'weight', ax[0])
+cams.Plot.scatter_individual(families, 'avg', 'specific_weight', ax[1])
 ax[0].set_yscale('log')
 ax[0].set_xscale('log')
 ax[1].set_yscale('log')
