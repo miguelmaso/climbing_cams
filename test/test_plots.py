0032b74290b7b74974a6ce80d734027b0c529ec6
miguelmaso <miguel.maso@upc.edu>
2024-10-12 18:47:27 +0200

    update and add testing

--- test/test_plots.py
+++ test/test_plots.py
@@ -3,41 +3,45 @@ import matplotlib as mpl
 import pytest
 
 
-def test_rack_barchart_fail():
+def test_rack_bar_chart_fail():
     with pytest.raises(Exception) as e_info:
-        cams.plots.rack_barchart(1)
-    assert str(e_info.value) == f'{cams.plots.rack_barchart} must be called with a {cams.rack.Rack} ' + \
+        cams.plots.rack_bar_chart(1)
+    assert str(e_info.value) == f'{cams.plots.rack_bar_chart} must be called with a {cams.rack.Rack} ' + \
                                 f'instance but it was called with {int}'
 
 
-def test_racks_barchart_fail():
+def test_racks_bar_chart_fail():
     with pytest.raises(Exception) as e_info:
-        cams.plots.racks_barchart([1])
-    assert str(e_info.value) == f'{cams.plots.racks_barchart} must be called with a list of {cams.rack.Rack} ' + \
+        cams.plots.racks_bar_chart([1])
+    assert str(e_info.value) == f'{cams.plots.racks_bar_chart} must be called with a list of {cams.rack.Rack} ' + \
                                 f'but it was called with a list of {int}'
 
 
-def test_rack_barchart():
+def test_rack_bar_chart():
     rack = cams.db.select(name="C4")
-    fig, ax = cams.plots.rack_barchart(rack)
+    fig, ax = cams.plots.rack_bar_chart(rack)
     assert type(fig) == mpl.figure.Figure
     assert type(ax) == mpl.axes._axes.Axes
 
 
-def test_racks_barchart():
+def test_racks_bar_chart():
     racks = [cams.db.select(**spec) for spec in [{'name': 'C4'}, {'name': 'UL'}]]
     assert len(racks) == 2
-    fig, ax = cams.plots.racks_barchart(racks)
+    fig, ax = cams.plots.racks_bar_chart(racks)
     assert type(fig) == mpl.figure.Figure
     assert len(ax) == 2
 
 
 def test_scatter_individual():
     racks = [cams.db.select(**spec) for spec in [{'name': 'C4'}, {'name': 'UL'}]]
-    assert len(racks) == 2
     fig, ax = cams.plots.scatter_individual(racks, 'avg', 'weight')
     assert type(fig) == mpl.figure.Figure
     assert type(ax) == mpl.axes._axes.Axes
+    assert len(ax.lines) == 2
+    line_c4 = ax.lines[0]
+    assert len(line_c4.get_data()[0]) == 10
+    line_ul = ax.lines[1]
+    assert len(line_ul.get_data()[0]) == 7
 
 
 def test_scatter_average():
@@ -46,4 +50,9 @@ def test_scatter_average():
     fig, ax = cams.plots.scatter_average(racks, 'avg', 'weight')
     assert type(fig) == mpl.figure.Figure
     assert type(ax) == mpl.axes._axes.Axes
+    assert len(ax.lines) == 2
+    line_c4 = ax.lines[0]
+    assert len(line_c4.get_data()[0]) == 1
+    line_ul = ax.lines[1]
+    assert len(line_ul.get_data()[0]) == 1
 
